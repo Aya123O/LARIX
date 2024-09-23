76d34dd9fcaf9ec59f5bfbf8d7dbc55a41220988
Zinab-salah <zinabsalah30@gmail.com>
2024-09-23 12:50:48 -0700

    hello

---
+++ index.js
@@ -0,0 +1,11 @@
+const elements = document.querySelectorAll('.element');
+
+elements.forEach((element) => {
+    element.addEventListener('click', () => {
+        
+        elements.forEach((el) => el.classList.remove('active'));
+
+       
+        element.classList.add('active');
+    });
+});
